976cb47c15fc0050d53e7e66ca3517ed02538707
Henry Lee <46310414+urijan44@users.noreply.github.com>
2021-11-08 22:37:32 +0900

    Initial Commit

---
+++ MemoMemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MemoMemo
+//
+//  Created by hoseung Lee on 2021/11/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
